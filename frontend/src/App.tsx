d5ac8d9aa1a7e1e41f4e835916667e458c37189e
Vinicius Montes <viniciusmontes@outlook.com.br>
2022-07-13 15:48:08 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
 
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 
 }
